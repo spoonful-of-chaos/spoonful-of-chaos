373230eaaa63123978c35274455dd093c9a525e4
spoonful-of-chaos <132939818+spoonful-of-chaos@users.noreply.github.com>
2023-05-10 19:59:59 -0400

    better class cards

--- src/pages/lou5e/characters.tsx
+++ src/pages/lou5e/characters.tsx
@@ -1,4 +1,6 @@
 import { Card } from "@/components/general/Card";
+import { classes } from "@/utils/classes";
+import Link from "next/link";
 
 const Characters = () => {
   return (
@@ -6,38 +8,40 @@ const Characters = () => {
       <h1 className="mb-2">Characters</h1>
       <div className="md:grid md:grid-cols-2 md:gap-8">
         <p>
-          Since The Last of Us takes place in a modern-day United States setting, many of the conventional rules/playstyles of Dungeons & Dragons are not applicable. The largest of these changes is the adjustment from spellcasting to crafting. To accommodate for this, the following classes have been created for the purpose of this campaign. 
+          Since The Last of Us takes place in a modern-day United States setting, many of the conventional rules/playstyles of Dungeons & Dragons are not applicable. The largest of these changes is the adjustment from spellcasting to crafting. To accommodate for this, the following classes have been created for the purpose of this campaign.
         </p>
         <p>
-        These are mostly reskins of existing classes, with tweaks thrown in to better suit the setting. To create a character, follow 5e character creation rules, bearing in mind that spells and cantrips have been replaced by a crafting system.
+          These are mostly reskins of existing classes, with tweaks thrown in to better suit the setting. To create a character, follow 5e character creation rules, bearing in mind that spells and cantrips have been replaced by a crafting system.
         </p>
       </div>
       <h4 className="mt-6">A Note on Playable Species</h4>
       <p>Because <span className="italics">The Last of Us</span> takes place in a modern, real-world setting, the only available playable species is human. Additionally, because there are no other playable species, do not take the additional ability score Human Trait from the Player&apos;s Handbook.</p>
       <h2 className="mt-12">Classes</h2>
       <div className="flex flex-col mt-8 md:grid md:grid-cols-3 md:gap-8">
-        <Card>
-          <h5>Smuggler</h5>
-          <p>A charismatic fighter with a few tricks up their sleeve.</p>
-          <ul>
-            <li><span className="font-black">Hit Die:</span> d8</li>
-            <li><span className="font-black">Primary Ability:</span> Charisma</li>
-            <li><span className="font-black">Saving Throw Proficiencies:</span> Dexterity & Charisma</li>
-            <li><span className="font-black">Armor & Weapon Proficiencies:</span> Handguns, rifles, simple weapons, hand-to-hand</li>
-          </ul>
-        </Card>
-        <Card>
-          <h5>Survivor</h5>
-        </Card>
-        <Card>
-          <h5>Prepper</h5>
-        </Card>
-        <Card>
-          <h5>Soldier</h5>
-        </Card>
-        <Card>
-          <h5>Bandit</h5>
-        </Card>
+        {Object.keys(classes)?.map((c: string, i: number) => (
+          <Card variant="ghost">
+            <h5 className="text-black">{c.toUpperCase()}</h5>
+            <p className="text-black">{classes[c].description}</p>
+            <table className="mt-4">
+              <tr>
+                <th>Hit Die</th>
+                <td>{classes[c].hd}</td>
+              </tr>
+              <tr>
+                <th>Primary Ability</th>
+                <td>{classes[c].primary}</td>
+              </tr>
+              <tr>
+                <th>Saving Throw Proficiencies</th>
+                <td>{classes[c].throw_prof}</td>
+              </tr>
+              <tr>
+                <th>Armor & Weapon Proficiencies</th>
+                <td>{classes[c].armor_prof}</td>
+              </tr>
+            </table>
+          </Card>
+        ))}
       </div>
     </>
   )
---
+++ src/utils/classes.ts
@@ -0,0 +1,37 @@
+export const classes: Record<string, Record<string, string>> = {
+  smuggler: {
+    description: 'A charismatic fighter with a few tricks up their sleeve.',
+    hd: 'd8',
+    primary: 'Charisma',
+    throw_prof: 'Dexterity & Charisma',
+    armor_prof: 'Handguns, rifles, simple weapons, hand-to-hand'
+  },
+  survivor: {
+    description: 'A master of survival within and outside of quarantine zones.',
+    hd: 'd10',
+    primary: 'Wisdom',
+    throw_prof: 'Strength & Wisdom',
+    armor_prof: 'Bow, simple weapons, hand-to-hand'
+  },
+  prepper: {
+    description: 'A long-term planner with an arsenal of traps, tricks, and other craftables.',
+    hd: 'd6',
+    primary: 'Intelligence',
+    throw_prof: 'Intelligence & Wisdom',
+    armor_prof: 'Handguns, rifles, simple weapons'
+  },
+  soldier: {
+    description: 'A combat specialist trained in a variety of abilities.',
+    hd: 'd10',
+    primary: 'Strength',
+    throw_prof: 'Strength & Constitution',
+    armor_prof: 'Handguns, mid-range firearms, simple weapons, bows, hand-to-hand'
+  },
+  bandit: {
+    description: 'A quick and quiet rogue who uses stealth and skill to solve problems.',
+    hd: 'd8',
+    primary: 'Dexterity',
+    throw_prof: 'Dexterity & Intelligence',
+    armor_prof: 'Simple weapons, knives, handguns'
+  }
+}

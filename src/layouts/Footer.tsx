cf42fe35813ba7c9dafe755061774aa51897287d
spoonful-of-chaos <132939818+spoonful-of-chaos@users.noreply.github.com>
2023-06-04 15:56:59 -0400

    updated with patreon link

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
       <footer className="w-full sticky bottom-0 mt-4 bg-black py-4 border-moonstone border-t-2">
-        <Link href="#" className="text-sm font-semibold text-moonstone hover:text-spoon">
+        <Link href="https://www.patreon.com/SpoonfulofChaos" target="_blank" className="text-sm font-semibold text-moonstone hover:text-spoon">
           Support me on Patreon <span aria-hidden="true">→</span>
         </Link>
       </footer>
